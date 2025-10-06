0a634529b9236805a78467803d7043484d1adaac
PengFeiMaWWX <37363732+PengFeiMaWWX@users.noreply.github.com>
2025-10-06 20:05:31 +0800

    Create variables-initializers.go

---
+++ code/src/hello/variables-initializers.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var i, j = 1, 2
+
+// 变量声明可以包含初始值，每个变量对应一个
+// 如果提供了初始值，那么类型可以省略
+func main() {
+
+	var c, python, java = true, 3.14, "hello"
+	fmt.Println(i, j, c, python, java)
+}
