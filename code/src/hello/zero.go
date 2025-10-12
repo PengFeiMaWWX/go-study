edd1cc7084a208d33b7a4a37f278b1569ac99213
PengFeiMaWWX <37363732+PengFeiMaWWX@users.noreply.github.com>
2025-10-12 21:47:41 +0800

    Create zero.go

---
+++ code/src/hello/zero.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+/**
+零值
+没有明确初始化的变量声明会被赋予对应类型的 零值。
+
+零值是：
+
+数值类型为 0，
+布尔类型为 false，
+字符串为 ""（空字符串）。
+ */
+func main() {
+
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Println("hello world")
+	fmt.Println(i, f, b, s)
+}
