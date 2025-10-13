c5b242329742efbba814b3a0c74a017cfff85e96
PengFeiMaWWX <37363732+PengFeiMaWWX@users.noreply.github.com>
2025-10-13 08:08:12 +0800

    Create forever.go

---
+++ code/src/flowController/forever.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/**
+ *
+  - 无限循环
+    for 循环的格式如下：
+    for {
+      循环体
+    }
+    这个 for 循环会一直执行循环体，直到被中断。
+*/
+func main() {
+
+	for {
+		fmt.Println("loop")
+	}
+
+}
