1660797feefb26445bbe42b1db4ee081657503fb
加了盐的咖啡 <605823850@qq.com>
2025-10-15 07:19:12 +0800

    4

---
+++ code/src/more-type/slices-len-cap.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/*
+*
+切片的长度与容量
+切片拥有 长度 和 容量。
+
+切片的长度就是它所包含的元素个数。
+
+切片的容量是从它的第一个元素开始数，到其底层数组元素末尾的个数。
+
+切片 s 的长度和容量可通过表达式 len(s) 和 cap(s) 来获取。
+
+你可以通过重新切片来扩展一个切片，给它提供足够的容量。 试着修改示例程序中的切片操作，向外扩展它的长度，看看会发生什么。
+
+len=6 cap=6 [2 3 5 7 11 13]
+len=0 cap=6 []
+len=4 cap=6 [2 3 5 7]
+len=2 cap=4 [5 7]
+*/
+func main() {
+
+	s := []int{2, 3, 5, 7, 11, 13}
+
+	printSlices(s)
+
+	// 截取切片使其长度变为0
+	s = s[:0]
+	printSlices(s)
+
+	// 扩展其长度\
+	s = s[:4]
+	printSlices(s)
+
+	//舍弃前两个值
+	s = s[2:]
+	printSlices(s)
+
+}
+
+func printSlices(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+
+}
